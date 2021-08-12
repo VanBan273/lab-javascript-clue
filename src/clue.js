81643c4f1c906045b398a87f41da5be5ae754594
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2021-08-12 15:50:39 +0200

    WD082021 - Iván Romero Oyola

--- src/clue.js
+++ src/clue.js
@@ -2,27 +2,60 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+  { firstName: 'Iván', lastName: 'Oyola', occupation: 'developer', age: 30, description:"hace cosas",color:"Black"},
+  { firstName: 'Adam', lastName: 'Romero', occupation: 'chef', age: 25, description:"parece que hace cosas", color:"green",},
+  { firstName: 'Elena', lastName: 'Rodriguez', occupation: 'actress', age:27, description:"hace algunas cosas", color:"pink,"},
+  { firstName: 'Yara', lastName: 'Tamayo', occupation: 'lawyer',age: 22 ,description:"hace alguna cosilla", color:"yellow"},
+  { firstName: 'Jesus', lastName: 'Olea', occupation: 'Entrepreneur',age: 21 ,description:"lo hace todo",color:"red"},
+  { firstName: 'Andrea', lastName: 'Lopez', occupation: 'teacher',age :26 ,description:"no hace nada", color:"blue"},
+],
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = ["Living room", "Study","Dining room", "Conservatory", "Library", "Billiard room",
+"Lounge", "Ballroom", "Kitchen","Hall", "A spa", "Observatory", "Theater", "Guest house", "A patio"],
 
 // Weapons Array
 
-const weaponsArray = [];
-
+const weaponsArray = [ {name:"Rope",weight:"2 kg",hurtScale:5},
+{name:"Knife",weight:"1 kg",hurtScale:8},
+{name:"Candlestick",weight:"3 kg",hurtScale:6},
+{name:"Dumbbell",weight:"4 kg",hurtScale:8},
+{name:"Poison",weight:"0.5 kg",hurtScale:10},
+{name:"Axe",weight:"3 kg",hurtScale:9},
+{name:"Bat",weight:"2 kg",hurtScale:6},
+{name:"Trophy",weight:"3 kg",hurtScale:8},
+{name:"Pistol",weight:"2.5 kg",hurtScale:10}];
+  
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arr) {
+    const selector = Math.floor(Math.random() * arr.length);
+    return arr[selector]
+  }
 
-function pickMystery() {}
+  function pickMystery() {
+    let myMystery = {
+     suspect:{}, weapon:{}, room:{} 
+    } 
+      myMystery.suspect = selectRandom(suspectsArray);
+      myMystery.weapon = selectRandom(weaponsArray);
+      myMystery.room = selectRandom(roomsArray);
+      return myMystery
+  }
 
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(obj) {
+    const firstName = obj.suspect.firstName;
+    const lastName = obj.suspect.lastName;
+    const weapon = obj.weapon.name;
+    const room = obj.room.name;
+    return ` ${firstName} ${lastName} killed Mr. Boddy using the ${weapon} in the ${room}!`
+  }
 
 
 
